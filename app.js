320d4abbe3074aee5836f7ded4f968fc0a9d1ab3
Shivam <srivashivam20@gmail.com>
2022-08-13 13:34:49 +0530

    server-setup

---
+++ app.js
@@ -0,0 +1,39 @@
+function getKnightMoves(cell)
+{
+    console.log(cell);
+    const s='ABCDEFGH';
+    const [x,y] =cell;
+   
+    
+    const axis =[
+        [1,2],
+        [2,1],
+        [-1,-2],
+        [-2,-1],
+        [1,-2],
+        [2,-1],
+        [-1,2],
+        [-2,1],
+    ];
+   axis.map(([a,b])=>
+    {
+    val=`${s[s.indexOf(x) + a]}${+y +b}`;
+     
+    [k,...j]=val;
+   if(j.length<2 )
+   {for(let l=0;l<8;l++)
+    if(j[l]<8)
+    {
+  document.getElementById(val).style.backgroundColor='#900'}
+   }
+ 
+
+})
+       
+    
+   }
+
+
+
+
+
